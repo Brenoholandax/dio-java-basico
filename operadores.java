502089fbda7d510c697c89e6ddc7f4d6ed4e536b
Brenoholandax <120995868+Brenoholandax@users.noreply.github.com>
2023-01-19 21:45:33 -0300

    Tipos e Variaveis
    Conseguir desenvolver e absolver o conteudo de tipos e variaveis em java

--- operadores.java
+++ operadores.java
@@ -1,13 +1,15 @@
 public class operadores {
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws Exception 
+    {
+          
+ String numeroUm = "EuSou";
+         String numeroDois = "Amo";
+         System.out.printLn(numeroUm.equals(numeroDois));
 
-int f3= 12;
-int  f2= 10;
-int j1 = 5;
-int j2 = 6;
-
-System.out.println((f3 > f2) || (j1 < j2) );
+ 
 
     }
     
 }
+    
+
